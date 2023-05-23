94590f3f695c752964604cc622a17ca20c2d64d1
Miledi75 <mileto.di.marco@howest.be>
2023-05-23 19:58:33 +0200

    Add create product

--- Pri.Vue.Web/wwwroot/js/Games/app.js
+++ Pri.Vue.Web/wwwroot/js/Games/app.js
@@ -11,8 +11,11 @@
             image: "",
             id: null,
             categories: null,
+        },
+        addProduct: {
+            name: "",
+            categories: null,
         }
-,
     },
     created: function () {
         this.getProducts();
@@ -40,5 +43,53 @@
             console.log("test");
             $("#productModal").modal("hide");
         },
+        showAddProductModal: async function () {
+            //get the categories
+            let categories = await axios.get("https://localhost:7047/api/categories").then(response => response.data.categories)
+                .catch(error => console.log(error.response.data))
+                .finally(() => { $("#addProductModal").modal('show'); })
+            console.log(categories);
+            //fill the addProduct categories
+            this.addProduct.categories = new Array();
+            categories.forEach(category => {
+                this.addProduct.categories.push({
+                    text: category.name,
+                    value: category.id,
+                    isSelected: false,
+                });
+            });
+            
+        },
+        hideAddProductModal: async function () {
+            //show the modal
+            $("#addProductModal").modal('hide');
+        },
+        createProduct: async function () {
+            //set the formdata
+            let formData = new FormData();
+            formData.append("Name", this.addProduct.name);
+            formData.append("Image", this.$refs.addImage.files[0]);
+            this.addProduct.categories.filter(cat => cat.isSelected === true)
+                .forEach((cat,index) => {
+                    formData.append(`categories[${index}]`,cat.value)
+                });
+            //set the headers
+            let headers = {
+                headers: {
+                    "Encoding-Type": "multipart/formdata",
+                    "Authorization": `bearer ${localStorage.token}`
+                }
+            }
+            //send the request
+            await axios.post("https://localhost:7047/api/games", formData, headers)
+                .then(response => {
+                    console.log(response.status);
+                    this.getProducts();
+                    this.hideAddProductModal();
+                    this.addProduct.name = "";
+                    this.$refs.addImage.value = "";
+                })
+                .catch(error => console.log(error.response.data))
+        }
     },
 });
